8675d98f775c740f130efba163876706ce97a3ab
Hrips03 <hripsimebarsegyan2003@gmail.com>
2025-03-07 17:38:44 +0400

    design patterns

--- Course_Project_Console/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "controller/controller.hpp"
-
-int main(){
-    system("clear");
-    Controller ctrl;
-    ctrl.run();
-}
--- Course_Project_Console/model/logic.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "../view/Grid.hpp"
-#include <cstdlib>
-#include <ctime> 
